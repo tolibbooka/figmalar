c59bb44f13f7c51f28bf6d076970ca4f20e9277a
tolibbooka <tgulomjanov1@gmail.com>
2022-08-14 17:35:50 +0500

    figma

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Orta from './Components/Orta';
+
+function App() {
+  return (
+    <div className="App">
+      <Orta/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Orta.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Orta = () => {
+    return (
+        <div className='max-width'>
+            <div className='ortasi1'>
+
+            <div className="ortasi">
+                <div className="bir">
+                    <img src="img/bir.png" alt="" />
+                </div>
+                <div className="yonalish">
+                    <a href="###">Design Things </a>
+                    <h1>We Loves To Design <br />
+                        Your Creative Ideas</h1>
+                    <p>A creative concept is an overarching “Big Idea” that <br /> captures audience interest, influences their emotional <br /> response and inspires them to take action.It is a unifying <br /> theme that can be used across all campaign messages </p>
+                    <button> Learn more <img src="img/button.png" alt="" /></button>
+                </div>
+            </div>
+            </div>
+
+
+            <div className="ikki">
+                <div className="ikki2">
+                    <a href="####">Our Teamwork </a>
+                    <h1>We Believe Success <br />
+                        Lies On Teamwork</h1>
+                    <p>Good teamwork means a synergistic way of working with <br /> each person committed and working towards a shared <br /> goal. ... It is therefore a necessity that leaders facilitate <br /> and build the teamwork skills of their people if they are</p>
+                    <button> Learn more <img src="img/button.png" alt="" /></button>
+                </div>
+                <div className="qator">
+                    <img src="img/ikki.png" alt="" />
+                </div>
+            </div>
+            <div className="uch">
+                <div className="bir1">
+                    <img src="img/uch.png" alt="" />
+                </div>
+                <div className="yonalish2">
+                    <a href="#####">Testimonial </a>
+                    <h1>We Loves To Design <br />
+                        Your Creative Ideas</h1>
+                    <p>A creative concept is an overarching “Big Idea” that <br /> captures audience interest, influences their emotional <br /> response and inspires them to take action.It is a unifying <br /> theme that can be used across all campaign messages </p>
+                    <h3>Jorge Morrison</h3>
+                    <p>North California,USA</p>
+                </div>
+            </div>
+
+            <div className="imgesss">
+                <div className="ugli">
+                    <img src="img/ferrari.png" alt="" />
+                </div>
+                <div className="ugli">
+                    <img src="img/microsoft.png" alt="" />
+                </div>
+                <div className="ugli">
+                    <img src="img/National.png" alt="" />
+                </div>
+                <div className="ugli">
+                    <img src="img/Deloitte.png" alt="" />
+                </div>
+                
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Orta;
